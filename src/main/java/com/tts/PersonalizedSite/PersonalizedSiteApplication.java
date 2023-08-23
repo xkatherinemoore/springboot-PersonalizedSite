80422060458edb2a12f92c5d2d76cf4a97a84091
katherinemoore <katherinemoore789@gmail.com>
2023-08-22 17:25:20 -0700

    First commit; upload from local repo

---
+++ src/main/java/com/tts/PersonalizedSite/PersonalizedSiteApplication.java
@@ -0,0 +1,13 @@
+package com.tts.PersonalizedSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalizedSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalizedSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/PersonalizedSite/PersonalizedSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.PersonalizedSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalizedSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
